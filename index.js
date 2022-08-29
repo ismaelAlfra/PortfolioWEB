3b8bcb5198f25edb2394ce4efd2ac01ad59b6d00
ismaelAlfra <ismaelalfra97@gmail.com>
2022-08-29 12:01:17 -0300

    upgrade

---
+++ index.js
@@ -0,0 +1,23 @@
+var linkIndex = document.querySelectorAll(".link-index");
+var icons = document.querySelectorAll(".icons");
+var body = document.querySelector('body');
+console.log(icons);
+for (i = 0; i < linkIndex.length; i++) {
+    linkIndex[i].addEventListener('click', function() {
+        console.log('click');
+        body.style.opacity = '0';
+        // icons.forEach(function(element) {
+        //     element.style.opacity = '0';
+        // }); {
+
+        // }
+    })
+    linkIndex[2].addEventListener('click', function() {
+        body.style.opacity = '1';
+        alert('Em construção');
+    })
+}
+
+function delay(URL) {
+    setTimeout(function() { window.location = URL }, 800);
+}
